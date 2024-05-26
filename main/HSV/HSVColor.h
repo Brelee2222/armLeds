b7e515e95d9d2038dd35b177620ea0841e386417
brelee2222 <brelee1842@outlook.com>
2024-05-25 18:11:20 -0700

    Restructuring

---
+++ main/HSV/HSVColor.h
@@ -0,0 +1,15 @@
+struct HSVColor {
+    const unsigned short hue;
+    const unsigned char saturation;
+    const unsigned char value;
+
+    HSVColor(
+        unsigned short hue,
+        unsigned char saturation,
+        unsigned char value
+    );
+
+    HSVColor();
+
+    
+};
--- main/main.ino
+++ main/main.ino
@@ -1,23 +1,9 @@
-#include "LEDControl.h"
-
-#define FRAME_DELAY 10
+#include "HSV/HSVColor.h"
 
 void setup() {
-    // Serial.begin(9600);
 
-    LEDControl::begin();
 }
 
 void loop() {
-    // void* ptr = malloc(1);
 
-    // Serial.println((int) ptr);
-
-    // free(ptr);
-    // delay(1000);
-    LEDControl::update();
-
-    LEDControl::display();
-
-    delay(FRAME_DELAY);
 }
